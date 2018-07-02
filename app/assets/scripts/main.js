128168c37b176b12f932b98813b2f559dd24e60a
Radek <sliwa.radoslaw2@gmail.com>
2018-07-02 21:08:59 +0200

    gg

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,13 +1,13 @@
 
 
-$(function(){
-  $('.banner__side-offer').delay(1000).fadeIn(800);
-});
-
-$(".banner__side-offer__close").click(()=>{
-  $('.banner__side-offer').css({display: 'none'});
-
-})
+// $(function(){
+//   $('.banner__side-offer').delay(1000).fadeIn(800);
+// });
+//
+// $(".banner__side-offer__close").click(()=>{
+//   $('.banner__side-offer').css({display: 'none'});
+//
+// })
 
 
 
@@ -33,13 +33,74 @@ class RevealOnScroll{
           $('.text-container__why-list').children().each(function(index){
             $(this).addClass('animated bounceInLeft');
           })
-          },
+        },
 
         offset: offset
       });
     });
   }
-
 }
 
 new RevealOnScroll($('.section'), "85%");
+
+
+//-------------------------------------------STICKY HEADER
+class StickyH{
+  constructor(){
+    this.siteHeader = $('.site-header');
+    this.triggerForHeader = $('.banner__btn-container');
+    this.headerLogo = $('.main-nav__logo');
+    this.headerLinks =$('.main-nav a');
+    this.pageSections = $('.section');
+    this.wayPointHeader();
+    this.sectionWaypoint();
+    }
+
+  wayPointHeader(){
+    new Waypoint({
+      element: this.triggerForHeader.get(0),
+      handler: (direction)=>{
+        if(direction == "down"){
+          this.siteHeader.addClass('site-header--darkerBackground');
+          this.headerLogo.addClass('main-nav__logo--smallerScale');
+        }else{
+          this.siteHeader.removeClass('site-header--darkerBackground');
+          this.headerLogo.removeClass('main-nav__logo--smallerScale');
+        }
+      },
+      offset: "35%"
+    });
+  }
+
+  sectionWaypoint(){
+    var that = this;
+    this.pageSections.each(function(){
+      var currentSection = this;
+      new Waypoint({
+        element: currentSection,
+        handler: function(direction){
+          if(direction =="down"){
+            var matchingLink = currentSection.getAttribute('data-match');
+            that.headerLinks.removeClass('matching-link');
+            $(matchingLink).addClass('matching-link');
+          }
+        },
+        offset: "20%"
+      });
+      new Waypoint({
+        element: currentSection,
+        handler: function(direction){
+          if(direction =="up"){
+            var matchingLink = currentSection.getAttribute('data-match');
+            that.headerLinks.removeClass('matching-link');
+            $(matchingLink).addClass('matching-link');
+          }
+        },
+        offset: "-40%"
+      });
+
+    })
+  }
+}
+
+new StickyH();
--- app/temp/scripts/main.js
+++ app/temp/scripts/main.js
@@ -4,13 +4,15 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-$(function () {
-  $('.banner__side-offer').delay(1000).fadeIn(800);
-});
+// $(function(){
+//   $('.banner__side-offer').delay(1000).fadeIn(800);
+// });
+//
+// $(".banner__side-offer__close").click(()=>{
+//   $('.banner__side-offer').css({display: 'none'});
+//
+// })
 
-$(".banner__side-offer__close").click(function () {
-  $('.banner__side-offer').css({ display: 'none' });
-});
 
 var RevealOnScroll = function () {
   function RevealOnScroll(elements, offset) {
@@ -50,3 +52,74 @@ var RevealOnScroll = function () {
 }();
 
 new RevealOnScroll($('.section'), "85%");
+
+//-------------------------------------------STICKY HEADER
+
+var StickyH = function () {
+  function StickyH() {
+    _classCallCheck(this, StickyH);
+
+    this.siteHeader = $('.site-header');
+    this.triggerForHeader = $('.banner__btn-container');
+    this.headerLogo = $('.main-nav__logo');
+    this.headerLinks = $('.main-nav a');
+    this.pageSections = $('.section');
+    this.wayPointHeader();
+    this.sectionWaypoint();
+  }
+
+  _createClass(StickyH, [{
+    key: 'wayPointHeader',
+    value: function wayPointHeader() {
+      var _this = this;
+
+      new Waypoint({
+        element: this.triggerForHeader.get(0),
+        handler: function handler(direction) {
+          if (direction == "down") {
+            _this.siteHeader.addClass('site-header--darkerBackground');
+            _this.headerLogo.addClass('main-nav__logo--smallerScale');
+          } else {
+            _this.siteHeader.removeClass('site-header--darkerBackground');
+            _this.headerLogo.removeClass('main-nav__logo--smallerScale');
+          }
+        },
+        offset: "35%"
+      });
+    }
+  }, {
+    key: 'sectionWaypoint',
+    value: function sectionWaypoint() {
+      var that = this;
+      this.pageSections.each(function () {
+        var currentSection = this;
+        new Waypoint({
+          element: currentSection,
+          handler: function handler(direction) {
+            if (direction == "down") {
+              var matchingLink = currentSection.getAttribute('data-match');
+              that.headerLinks.removeClass('matching-link');
+              $(matchingLink).addClass('matching-link');
+            }
+          },
+          offset: "20%"
+        });
+        new Waypoint({
+          element: currentSection,
+          handler: function handler(direction) {
+            if (direction == "up") {
+              var matchingLink = currentSection.getAttribute('data-match');
+              that.headerLinks.removeClass('matching-link');
+              $(matchingLink).addClass('matching-link');
+            }
+          },
+          offset: "-40%"
+        });
+      });
+    }
+  }]);
+
+  return StickyH;
+}();
+
+new StickyH();
